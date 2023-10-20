73f9f4b4c7116e94eec081b97fc24688c26776f5
Exanite <exanite78012@gmail.com>
2023-10-20 19:01:08 -0400

    Add player movement

--- Jam Game/Assets/Scripts/Player/PlayerMovement.cs
+++ Jam Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Dependencies")]
+    [SerializeField] private Rigidbody2D rb;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Configuration")]
+    [SerializeField] private float movementSpeed = 8f;
+    [SerializeField] private float movementSpeedSmoothTime = 0.03f;
+
+    private Vector2 velocitySmoothing;
+
+    private void Update()
     {
-        
+        var targetVelocity = Vector2.zero;
+        targetVelocity.x -= Input.GetKey(KeyCode.A) ? 1 : 0;
+        targetVelocity.x += Input.GetKey(KeyCode.D) ? 1 : 0;
+        targetVelocity.y += Input.GetKey(KeyCode.W) ? 1 : 0;
+        targetVelocity.y -= Input.GetKey(KeyCode.S) ? 1 : 0;
+
+        targetVelocity = Vector2.ClampMagnitude(targetVelocity, 1);
+
+        targetVelocity *= movementSpeed;
+
+        rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref velocitySmoothing, movementSpeedSmoothTime);
     }
 }
