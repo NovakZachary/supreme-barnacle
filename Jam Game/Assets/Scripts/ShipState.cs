430d42fc16f03f0aac0fa90eda10e026c072c7d2
Exanite <exanite78012@gmail.com>
2023-10-21 14:59:14 -0400

    Implement time tracking

--- Jam Game/Assets/Scripts/ShipState.cs
+++ Jam Game/Assets/Scripts/ShipState.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,9 +19,44 @@ public class ShipState : SingletonBehaviour<ShipState>
     [Range(-1, 1)]
     public float playerDrunkenessNoise = 0;
 
-    [Header("Player movement")]
+    [Header("Player movement")] // Note: This won't be rendered because stopPlayerMovementRequests is not serialized
     public HashSet<object> stopPlayerMovementRequests = new HashSet<object>();
 
     [Header("Ship movement")]
     public Vector2 shipSpeed = Vector2.zero;
+
+    [Header("Time")]
+    [Tooltip("Time survived.")]
+    public float timeSurvivedSeconds = 0;
+    [Tooltip("Determines how long an hour is.")]
+    public float secondsPerHour = 10; // 4 min or 10 seconds per hour
+    [Tooltip("The hour the game starts at. Useful for making the game start at night instead of day, etc.")]
+    public int startingHour = 8;
+
+    /// <summary>
+    /// The current time in hours. Range of [0, 24).
+    /// </summary>
+    public float currentDayTime => (timeSurvivedSeconds / secondsPerHour + startingHour) % 24;
+
+    /// <summary>
+    /// The current hour. Range of [0, 24).
+    /// </summary>
+    public int currentDayHour => (int)currentDayTime;
+
+    /// <summary>
+    /// The current minute. Range of [0, 60).
+    /// </summary>
+    public int currentDayMinutes => (int)(currentDayTime % 1 * 60);
+
+    /// <summary>
+    /// The current time of day, normalized to be in the range of [0, 1).
+    /// </summary>
+    public float currentDayTimeNormalized => currentDayTime / 24;
+
+    private void Update()
+    {
+        // Probably better to not update values from here, but it's a game jam
+
+        timeSurvivedSeconds += Time.deltaTime;
+    }
 }
