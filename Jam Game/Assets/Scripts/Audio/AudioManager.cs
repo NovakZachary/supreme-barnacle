d592f90784ab043b8d1b3e010865dfe3a3ccd2c6
NovakZachary <147088592+NovakZachary@users.noreply.github.com>
2023-10-21 13:50:41 -0400

    More work on Sound Scripts and Added Title mp3

--- Jam Game/Assets/Scripts/Audio/AudioManager.cs
+++ Jam Game/Assets/Scripts/Audio/AudioManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine.Audio;
+using System;
 using UnityEngine;
 
 
@@ -7,15 +8,23 @@ public class AudioManager : MonoBehaviour
 
     public Sound[] sounds;
 
-    // Start is called before the first frame update
-    void Start()
+    // Awake is called before Start
+    void Awake()
     {
-        
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //Searches through sounds[] and 
+    public void Play (string name)
     {
-        
+        Sound s = Array.Find(sounds, sound => sound.name == name); //Using anonymous function
+        s.source.Play(); //Will throw error if no sound found!
     }
 }
--- Jam Game/Assets/Scripts/Audio/Sound.cs
+++ Jam Game/Assets/Scripts/Audio/Sound.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 [System.Serializable]
 public class Sound {
-    
-    public AudioClip clip;
-    
+
+    public string name;
+
+    public AudioClip clip; //refernce to audioclip itself
+
+    [Range(0f, 1f)] //Range of sound volume
     public float volume;
+
+    [Range(.1f, 3f)] //Range of pich
     public float pitch;
 
+    [HideInInspector] //Don't want it cluttering at runtime esp since it's populated automatically
+    public AudioSource source;
 
 }
