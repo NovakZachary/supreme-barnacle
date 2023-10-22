0e78e524361dbe7e0ac3c37784d87e72a8f1c26a
ZemiArtifact <96932439+ZemiArtifact@users.noreply.github.com>
2023-10-22 19:36:02 -0400

    UI Script & UI redesign
    Redesigned the UI to fit current scope. Programmed some of the features for the UI.

--- Jam Game/Assets/Scripts/UI/UIInterface.cs
+++ Jam Game/Assets/Scripts/UI/UIInterface.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class UIInterface : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Day Night Indicator")]
+    public bool isNight;
+    public Image DayNightIndicator;
+    public Sprite dayImage;
+    public Sprite nightImage;
+
+    [Header("Event Indicator")]
+    public bool isEvent;
+    public GameObject eventIndicator;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isNight) 
+        {
+            DayNightIndicator.sprite = nightImage;
+        } else 
+        {
+            DayNightIndicator.sprite = dayImage;
+        }
+
+        if (isEvent) 
+        {
+            eventIndicator.SetActive(true);
+        } else 
+        {
+            eventIndicator.SetActive(false);
+        }
     }
 }
