6c10590931a9165553bca85d2cfb9b8de87a16d7
Exanite <exanite78012@gmail.com>
2023-10-23 19:33:24 -0400

    Fix WineBarrel applying drunkenness regardless of what collider hit it

--- Jam Game/Assets/Scripts/Objects/WineBarrel.cs
+++ Jam Game/Assets/Scripts/Objects/WineBarrel.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class WineBarrel : MonoBehaviour
-{
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        ShipState.Instance.playerDrunkeness = 1f;
-    }
-}
+public class WineBarrel : MonoBehaviour {}
--- Jam Game/Assets/Scripts/Player/PlayerMovementCollider.cs
+++ Jam Game/Assets/Scripts/Player/PlayerMovementCollider.cs
@@ -1,8 +1,38 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementCollider : MonoBehaviour
 {
     public PlayerMovement playerMovement;
+
+    // Easiest place to implement, but not a great idea
+    private HashSet<WineBarrel> wineBarrels = new();
+
+    private void Update()
+    {
+        wineBarrels.RemoveWhere(barrel => !barrel);
+        if (wineBarrels.Count > 0)
+        {
+            ShipState.Instance.playerDrunkeness = 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.TryGetComponent(out WineBarrel wineBarrel))
+        {
+            wineBarrels.Add(wineBarrel);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.TryGetComponent(out WineBarrel wineBarrel))
+        {
+            wineBarrels.Remove(wineBarrel);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
