091631b2955c26a8cd71b2dab3861c2a69cd3098
Exanite <exanite78012@gmail.com>
2023-10-23 16:14:21 -0400

    Water buckets now create a puddle when thrown

--- Jam Game/Assets/Scripts/Items/WaterBucketProjectile.cs
+++ Jam Game/Assets/Scripts/Items/WaterBucketProjectile.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class WaterBucketProjectile : MonoBehaviour
 {
+    [SerializeField] private GameObject puddlePrefab;
     [SerializeField] private Transform bucket;
 
-    [SerializeField] private float maxDistance = 10f;
+    [SerializeField] private float maxDistance = 3f;
+    [SerializeField] private float puddleSpawnDistance = 1.5f;
     [SerializeField] private float speed = 10f;
     [SerializeField] private float rotationSpeed = 45f;
 
     private float distanceTraveled = 0;
+    private bool hasPuddleSpawned = false;
 
     private void Update()
     {
@@ -17,6 +20,12 @@ public class WaterBucketProjectile : MonoBehaviour
 
         transform.position += transform.right * distance;
         bucket.transform.rotation *= Quaternion.Euler(0, 0, rotationSpeed * Time.deltaTime);
+
+        if (!hasPuddleSpawned && distanceTraveled > puddleSpawnDistance)
+        {
+            hasPuddleSpawned = true;
+            Instantiate(puddlePrefab, transform.position, Quaternion.identity);
+        }
 
         if (distanceTraveled > maxDistance)
         {
