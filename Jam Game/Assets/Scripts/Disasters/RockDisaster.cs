313f510ae35264ca0cca21b2d1eca934e5b85171
Exanite <exanite78012@gmail.com>
2023-10-23 20:29:09 -0400

    Edit rock balance

--- Jam Game/Assets/Scripts/Disasters/RockDisaster.cs
+++ Jam Game/Assets/Scripts/Disasters/RockDisaster.cs
@@ -1,71 +1,47 @@
 using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class RockDisaster : MonoBehaviour
 {
-    [Header("Dependencies")] 
+    [Header("Dependencies")]
     [SerializeField] private FloatingRocks rockPrefab;
     [SerializeField] private Transform leftCreationPoint;
     [SerializeField] private Transform rightCreationPoint;
-    
-    [Header("Configuration")]
-    [SerializeField] private float minTimeDuringWaves = 20;
-    [SerializeField] private float maxTimeDuringWaves = 60;
-    [SerializeField] private float spawnInterval = 20;
 
-    [SerializeField] private float minDistanceBetweenRocks = 15;
-    [SerializeField] private float maxDistanceBetweenRocks = 40;
-    
+    [Header("Configuration")]
+    [SerializeField] private float minRockCount = 5;
+    [SerializeField] private float maxRockCount = 15;
+
     [SerializeField] private float minRockSize = 2;
     [SerializeField] private float maxRockSize = 10;
 
-    [SerializeField] private float minTimeBetweenWaves = 30;
-    [SerializeField] private float maxTimeBetweenWaves = 60;
+    [SerializeField] private float timeBetweenWaves = 60;
+    [SerializeField] private float timeBetweenWavesVariance = 5f;
 
-    private float previousTimeDuringWave = 0;
-    private float timeDuringWave;
     private float timeUntilNextWave;
 
-    private List<FloatingRocks> rocks = new List<FloatingRocks>();
-
-    private void Start()
-    {
-        StartNextWave();
-    }
+    private readonly List<FloatingRocks> rocks = new();
 
     private void Update()
     {
-        timeDuringWave -= Time.deltaTime;
-        if (timeDuringWave <= previousTimeDuringWave - spawnInterval && timeDuringWave >= 0)
+        timeUntilNextWave -= Time.deltaTime;
+        if (timeUntilNextWave < 0)
         {
             SpawnRocks();
-            previousTimeDuringWave = timeDuringWave;
-        }
 
-        timeUntilNextWave -= Time.deltaTime;
-        if (timeUntilNextWave <= 0)
-        {
-            StartNextWave();
+            timeUntilNextWave = timeBetweenWaves + Random.Range(-timeBetweenWavesVariance, timeBetweenWavesVariance);
         }
-    }
-
-    private void StartNextWave()
-    {
-        timeDuringWave = Random.Range(minTimeDuringWaves, maxTimeDuringWaves);
-        previousTimeDuringWave = timeDuringWave;
-        timeUntilNextWave = timeDuringWave + Random.Range(minTimeBetweenWaves, maxTimeBetweenWaves);
     }
 
     private void SpawnRocks()
     {
-        var maxDistance = Vector3.Distance(leftCreationPoint.localPosition,rightCreationPoint.localPosition);
-        var distance = 0f;
-        while (distance < maxDistance)
+        var rockCount = Random.Range(minRockCount, maxRockCount + 1);
+
+        for (var i = 0; i < rockCount; i++)
         {
             var randomScale = Random.Range(minRockSize, maxRockSize);
-            distance += Random.Range(minDistanceBetweenRocks, maxDistanceBetweenRocks) + randomScale;
-            var spawnPoint = Vector3.Lerp(leftCreationPoint.localPosition, rightCreationPoint.localPosition, distance/maxDistance);
+            var spawnPoint = Vector3.Lerp(leftCreationPoint.localPosition, rightCreationPoint.localPosition, Random.value);
+
             var rock = Instantiate(rockPrefab, spawnPoint, Quaternion.identity);
             rocks.Add(rock);
             rock.transform.localScale = new Vector3(randomScale, randomScale, 0);
